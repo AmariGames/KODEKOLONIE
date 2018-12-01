e47a10fde5b1e0a7560eebd8f0c0c95001bcf24d
ISSBDSCM <45515911+ISSBDSCM@users.noreply.github.com>
2018-12-02 00:55:19 +0900

    1812020055유닛컨트롤러

--- UnitController.cs
+++ UnitController.cs
@@ -6,9 +6,11 @@ public class UnitController : MonoBehaviour {
     public float speed;
     public Rigidbody2D rb2d;
     Vector2 pos;
+    bool flag;
     void Awake()
     {
         rePos();
+        flag = false;
     }
     void FixedUpdate()
     {
@@ -18,19 +20,27 @@ public class UnitController : MonoBehaviour {
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
         */
-        if (Input.GetMouseButton(0)) pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            flag = true;
+        }
+
         //if ((Mathf.Abs(pos.x - transform.position.x) < 5) && (Mathf.Abs(pos.y - transform.position.y) < 5)) rePos();
-            //transform.position = new Vector3(pos.x,pos.y,transform.position.z);
+        //transform.position = new Vector3(pos.x,pos.y,transform.position.z);
         /* 
          float angle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
          Quaternion rotate = Quaternion.AngleAxis(angle, Vector3.forward);
          transform.rotation = Quaternion.Slerp(transform.rotation, rotate, 5f * Time.deltaTime);
          */
-
-        Vector2 rotate = new Vector2(pos.x - transform.position.x, pos.y - transform.position.y);
-        transform.up = rotate;
-        transform.position += transform.up * speed * Time.deltaTime;
-        
+        if (flag)
+        {
+            Vector2 rotate = new Vector2(pos.x - transform.position.x, pos.y - transform.position.y);
+            transform.up = rotate;
+            transform.position += transform.up * speed * Time.deltaTime;
+            isCloseToMouse();
+        }
 
       //  transform.position += new Vector3(((pos.x - transform.position.x) * speed * Time.deltaTime ), ((pos.y - transform.position.y) * speed * Time.deltaTime), 0);
       
@@ -38,5 +48,10 @@ public class UnitController : MonoBehaviour {
     void rePos()
     {
         pos = transform.position;
+    }
+    void isCloseToMouse()
+    {
+        if ((Mathf.Abs(pos.x - transform.position.x) < 0.5) && (Mathf.Abs(pos.y - transform.position.y) < 0.5))
+            flag = false;
     }
 }
